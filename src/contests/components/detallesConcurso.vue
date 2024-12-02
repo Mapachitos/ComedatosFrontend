4a734fdecb435a54e9958178d4797e722293ccf5
YadiMedina <131190334+YadiMedina@users.noreply.github.com>
2024-12-02 04:30:26 -0500

    moy

---
+++ src/contests/components/detallesConcurso.vue
@@ -0,0 +1,127 @@
+<template>
+    <div class="w-full h-full p-8 space-y-6 min-w-120 overflow-y-auto scroll-tracking rounded-lg">
+
+        <div class="w-full flex items-center flex-row">
+            <h1 class="text-xl font-bold text-color-2 flex-grow sm:text-center text-start flexr">
+                Nombre del Concurso
+                <i class="pi pi-trophy mr-2 text-yellow-500"></i>
+            </h1>
+            <button
+                class="justify-end px-3 py-2 text-white active:text-gray-200 bg-color-2 hover:brightness-125 active:brightness-75 font-medium rounded-md">
+                Editar
+            </button>
+        </div>
+
+        <div class="bg-white p-8 rounded-lg shadow-lg border border-gray-300">
+            <div class="gap-y-6 w-full flex flex-col">
+
+                <!-- Descripción -->
+                <div class="flex flex-col w-full">
+                    <label for="descripcionProyecto"
+                        class="text-lg font-semibold text-gray-700 mb-2">Descripción:</label>
+                    <div class="px-3 py-2 bg-gray-100 rounded-md">
+                        <h1
+                            class="text-sm sm:text-base 3xl:text-lg font-bold text-gray-800 w-full text-wrap break-words text-justify">
+                            holaaaa</h1>
+                    </div>
+                </div>
+
+                <!-- Área -->
+                <div class="flex flex-col w-full">
+                    <label for="nombreProyecto" class="text-lg font-semibold text-gray-900 mb-2">Área:</label>
+                    <div class="px-3 py-2 bg-gray-100 rounded-md">
+                        <h1
+                            class="text-sm sm:text-base 3xl:text-lg font-bold text-gray-800 w-full text-wrap break-words text-justify">
+                            holaaaa</h1>
+                    </div>
+                </div>
+
+                <!-- Puestos -->
+                <div class="flex flex-col w-full">
+                    <label for="puestos" class="text-lg font-semibold text-gray-900 mb-2">Puesto:</label>
+                    <div class="px-3 py-2 bg-gray-100 rounded-md">
+                        <h1
+                            class="text-sm sm:text-base 3xl:text-lg font-bold text-gray-800 w-full text-wrap break-words text-justify">
+                            holaaaa</h1>
+                    </div>
+                </div>
+
+                <!-- Ubicación y Sueldo -->
+                <div class="flex flex-col md:flex-row md:space-x-6">
+                    <div class="flex flex-col w-full md:w-3/5">
+                        <label for="ubicacion" class="text-lg font-semibold text-gray-900 mb-2">Ubicación:</label>
+                        <div class="px-3 py-2 bg-gray-100 rounded-md">
+                            <h1
+                                class="text-sm sm:text-base 3xl:text-lg font-bold text-gray-800 w-full text-wrap break-words text-justify">
+                                holaaaa</h1>
+                        </div>
+                    </div>
+                    <div class="flex flex-col w-full md:w-2/5 mt-4 md:mt-0">
+                        <label for="sueldo" class="text-lg font-semibold text-gray-900 mb-2">Sueldo:</label>
+                        <div class="px-3 py-2 bg-gray-100 rounded-md">
+                            <h1
+                                class="text-sm sm:text-base 3xl:text-lg font-bold text-gray-800 w-full text-wrap break-words text-justify">
+                                holaaaa</h1>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Requisitos -->
+                <div class="flex flex-col w-full">
+                    <label for="requisitosProyecto" class="text-lg font-semibold text-gray-900 mb-2">Requisitos:</label>
+                    <div class="px-3 py-2 bg-gray-100 rounded-md">
+                        <h1
+                            class="text-sm sm:text-base 3xl:text-lg font-bold text-gray-800 w-full text-wrap break-words text-justify">
+                            holaaaa</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Botones fuera del formulario -->
+        <div class="flex justify-around space-x-4">
+            <button @click="storeNav.setSelectedView('ListaConcursos');"
+                class="pi pi-arrow-left px-3 py-2 text-white active:text-gray-200 bg-color-3 hover:brightness-125 active:brightness-75 font-medium rounded-md fixed-button">
+            </button>
+        </div>
+
+        <detallesEtapa />
+        
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { navInfoStore } from '@/stores/NavInfoStore';
+// Componentes
+import detallesEtapa from './detallesEtapa.vue';
+
+// Store 
+const storeNav = navInfoStore();
+</script>
+
+<style scoped>
+.scroll-tracking::-webkit-scrollbar {
+    width: 8px;
+}
+
+.scroll-tracking::-webkit-scrollbar-track {
+    background: #f1f1f1;
+    border-radius: 10px;
+}
+
+.scroll-tracking::-webkit-scrollbar-thumb {
+    background: #cac9c9;
+    border-radius: 10px;
+}
+
+.scroll-tracking::-webkit-scrollbar-thumb:hover {
+    background: #807f7f;
+}
+
+.fixed-button {
+    position: fixed;
+    bottom: 52px;
+    right: 25px;
+}
+</style>
---
+++ src/contests/components/detallesEtapa.vue
@@ -0,0 +1,25 @@
+<template>
+      <div class="flex flex-col p-8 w-full rounded-xl shadow-lg border border-gray-300 bg-white">
+            <div class="w-full text-center mb-4 flex flex-row justify-between items-center">
+                <h1 class="text-lg md:text-xl text-gray-800 text-center flex-1">
+                    <span class="font-bold text-color-2">Etapa 1</span>
+                </h1>
+                <button
+                    class="px-3 py-2 text-white active:text-gray-200 bg-color-2 hover:brightness-125 active:brightness-75 font-medium rounded-md">
+                    Evaluar
+                </button>
+            </div>
+
+            <!-- Información del Concurso -->
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <div class="p-4 bg-gray-100 rounded-lg shadow-md">
+                    <p class="text-gray-600"><span class="font-bold text-gray-800">Nombre de la Etapa:</span> </p>
+                    <p class="text-gray-600"><span class="font-bold text-gray-800">Estatus:</span> </p>
+                </div>
+                <div class="p-4 bg-gray-100 rounded-lg shadow-md">
+                    <p class="text-gray-600"><span class="font-bold text-gray-800">Fecha de Registro:</span> </p>
+                    <p class="text-gray-600"><span class="font-bold text-gray-800">Fecha de Cierre:</span> </p>
+                </div>
+            </div>
+        </div>
+</template>
